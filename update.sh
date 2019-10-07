bee104643ca1bb6ce772c4c4f8b7e7026138596e
Franck M <franckisys@gmail.com>
2019-10-07 18:08:14 +0200

    Global change

--- update.sh
+++ update.sh
@@ -1,41 +1,31 @@
 #!/bin/bash
 
-usage() {
-    cat >&3 <<EOF
-Usage: $0 <Last IP address> <New IP address> <Pluging Device>
-Change the ip address of the Wazo Phonebook
+NewIP=$(whiptail --title "Hello Wazo HELP / IP PhoneBook" --inputbox "Please insert new IP address" 10 60 1.1.1.1 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+{
+    echo -e "XXX\n0\nUpdate Please Wait... \nXXX"
+<< EOF wazo-provd-cli
+base = configs.get('base')
+base['raw_config']['X_xivo_phonebook_ip'] = '$NewIP'
+configs.update(base)
+exit()
 EOF
-	exit 1
-}
+    sleep 1
+    echo -e "XXX\n50\nUpdate IP... Done.\nXXX"
+    sleep 0.5
 
-if [ "$#" -ne 3 ]; then
-   usage
+    echo -e "XXX\n50\nRestart services... \nXXX"
+    systemctl restart wazo-provd wazo-phoned
+
+    echo -e "XXX\n100\nRestart services... Done.\nXXX"
+    sleep 0.5
+
+} | whiptail --gauge "Veuillez patienter pendant l'installation" 6 60 0
+
+whiptail --title "Hello Wazo HELP / IP PhoneBook" --msgbox "Please reset or re-provision your devices\nHave a nice day :-) Wazo Support." 10 60
+
+else
+    echo "Y've cancel... :-("
 fi
-
-replace() {
-    local search=$1
-    local replace=$2
-    local using_pluging=$3
-    # Note the double quotes
-    echo "Last IP : ${search} / New IP : ${replace}"
-    echo "Replace..."
-    sed -i "s/${search}/${replace}/g" /var/lib/wazo-provd/jsondb/configs/base
-    echo "Replaced, result :"
-    cat /var/lib/wazo-provd/jsondb/configs/base
-    clean ${using_pluging}
-}
-
-clean() {
-    echo "Restart Wazo-Provd"
-    systemctl restart wazo-provd
-    echo "Done"
-    echo "Reconfigure Devices Configuration ${using_pluging}"
-    wazo-provd-cli -c 'devices.using_plugin("${using_pluging}").reconfigure()'
-    echo "Done"
-    echo "Synchronysing Devices ${using_pluging}"
-    wazo-provd-cli -c 'devices.using_plugin("${using_pluging}").synchronize()'
-    echo "Done"
-    echo "All is good, bye"
-}
-
-replace ${1} ${2} ${3}
